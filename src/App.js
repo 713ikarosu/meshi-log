5d75f90e26237b30534983d05e6d5390606c83ed
soudahirokatsu <>
2019-04-16 15:07:49 +0900

    初期化

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <p>
+            WAROTA
           </p>
           <a
             className="App-link"
